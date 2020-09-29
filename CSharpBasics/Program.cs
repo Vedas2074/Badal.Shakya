10a957ae98a8169526a83fc8df6ce63acf81a7cd
Badal Shakya <badal.s@outlook.com>
2020-09-29 09:43:26 +0545

    Added CSharpBasics Project

---
+++ CSharpBasics/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+// namespace CSharpBasics
+// {
+//     class Program
+//     {
+//         static void Main(string[] args)
+//         {
+//             Console.WriteLine("Hello World!");
+//             Console.Write("Enter your name: ");
+//             string name = Console.ReadLine();
+//             Console.WriteLine("Hi, "+ name);
+//         }
+//     }
+// }
+
+// class Program
+// {
+//     static void Main()
+//     {
+//         SelectionStatement selectionStatement = new SelectionStatement();
+//         selectionStatement.LearnSwitchCases();
+        
+//     }
+// }
+
+class Program
+{
+    static void Main123()
+    {
+        IterationStatement iterationStatement = new IterationStatement();
+        iterationStatement.LearnForLoops();
+    }
+}
